a7cb4f2396b4c17ed22b4750aa83bfcfc896f9dd
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2022-05-18 12:58:21 -0300

    Initial PassPhraseGenerator implementation

--- src/main/kotlin/passphrase/PassphraseGenerator.kt
+++ src/main/kotlin/passphrase/PassphraseGenerator.kt
@@ -16,11 +16,50 @@
 package br.com.colman.passphrase
 
 import br.com.colman.passphrase.WordGenerator
+import java.util.Locale
+import kotlin.random.Random
+import com.soywiz.krypto.SecureRandom as KryptoSecureRandom
 
 
 public class PassphraseGenerator(
-  wordGenerator: WordGenerator
+  private val random: Random = KryptoSecureRandom,
+  private val wordGenerator: WordGenerator = WordGenerator(random = random)
 ) {
 
+  public fun generate(wordAmount: Int = 3, separator: String = "-", includeNumber: Boolean = true, titleCaseWords: Boolean = true): String {
+    val words = wordGenerator.generateWords(wordAmount)
 
+    val wordsWithNumbers = if(includeNumber) {
+      words.includeRandomNumberAtEndOfRandomWord()
+    } else words
+
+    val capitalizedWords = if(titleCaseWords) {
+      wordsWithNumbers.capitalizeAll()
+    } else wordsWithNumbers
+
+    return capitalizedWords.joinToString(separator)
+  }
+
+  private fun List<String>.capitalizeAll() = map { it.capitalized() }
+  private fun List<String>.includeRandomNumberAtEndOfRandomWord(): List<String> {
+    val randomNumber = random.nextInt(0, 10)
+    val randomIndex = random.nextInt(0, size)
+
+    return mapIndexed { index, s -> if(index == randomIndex) s + randomNumber else s }
+  }
+
+  public companion object {
+    public val Default: PassphraseGenerator = PassphraseGenerator()
+  }
+}
+
+/**
+ * Replacement for Kotlin's deprecated `capitalize()` function.
+ */
+private fun String.capitalized(): String {
+  return this.replaceFirstChar {
+    if (it.isLowerCase())
+      it.titlecase(Locale.getDefault())
+    else it.toString()
+  }
 }
--- src/test/kotlin/passphrase/PassphraseGeneratorSpec.kt
+++ src/test/kotlin/passphrase/PassphraseGeneratorSpec.kt
@@ -17,24 +17,94 @@
 
 package br.com.colman.passphrase
 
-import br.com.colman.passphrase.WordGenerator
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.inspectors.forAll
+import io.kotest.inspectors.forNone
+import io.kotest.matchers.char.shouldBeInRange
+import io.kotest.matchers.collections.shouldContainAll
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
-import java.lang.ClassLoader.getSystemResourceAsStream as resource
+import io.kotest.matchers.string.shouldNotContainADigit
+import io.kotest.matchers.string.shouldStartWith
+import kotlin.random.Random
 
 class PassphraseGeneratorSpec : FunSpec({
 
-  test("Default word list to EFF big word list") {
-    val wordlist = resource("eff_large_wordlist.txt").bufferedReader().readLines()
+  test("Sanity check") {
+    val random = Random(19980209)
+    val generator = PassphraseGenerator(random)
 
-    WordGenerator.words shouldBe wordlist
+    val results = List(4) { generator.generate(3, "&") }
+
+    results[0] shouldBe "Preppy&Resilient&Mutiny1"
+    results[1] shouldBe "Scaling4&Diffusion&Prominent"
+    results[2] shouldBe "Sake&Cocoa&Passivism9"
+    results[3] shouldBe "Dingbat7&Disrupt&Mandolin"
   }
 
-  test("Includes a separator between the words") {
-    1 shouldBe 1
-  }
+  context("Default Generator") {
+    val defaultPassphraseGenerator = PassphraseGenerator.Default
 
-  test("Defaults to hyphen (-) as the separator") {
-    1 shouldBe 1
+    test("Generate passphrases using words from the big f list") {
+      val generatedWords = defaultPassphraseGenerator.generate(titleCaseWords = false).findWords()
+
+      BigWordList shouldContainAll generatedWords
+    }
+
+    test("Generate 3 words by default") {
+      val generated = defaultPassphraseGenerator.generate()
+
+      generated.findWords() shouldHaveSize 3
+    }
+
+    test("Generate as many words as I want") {
+      val generated = defaultPassphraseGenerator.generate(wordAmount = 18)
+      generated.findWords() shouldHaveSize 18
+    }
+
+    test("Separates words with hyphens by default") {
+      val generated = defaultPassphraseGenerator.generate()
+      val separators = generated.findSeparators()
+      separators shouldBe listOf("-", "-")
+    }
+
+    test("Lets me separate words with a custom separator") {
+      val generated = defaultPassphraseGenerator.generate(separator = "*")
+      val separators = generated.findSeparators()
+      separators shouldBe listOf("*", "*")
+    }
+
+    test("Adds a random number at the end of one of the words") {
+      val generated = defaultPassphraseGenerator.generate()
+      val number = generated.single { it.isDigit() }
+
+      number shouldBeInRange '0'..'9'
+    }
+
+    test("Allows me to disable numbers") {
+      val generated = defaultPassphraseGenerator.generate(includeNumber = false)
+      generated.shouldNotContainADigit()
+    }
+
+    test("Capitalizes words") {
+      val generated = defaultPassphraseGenerator.generate()
+      val words = generated.findWords()
+      words.forAll {
+        it shouldStartWith capitalLetterRegex
+      }
+    }
+
+    test("Allows me to disable word capitalization") {
+      val generated = defaultPassphraseGenerator.generate(titleCaseWords = false)
+      val words = generated.findWords()
+      words.forNone {
+        it shouldStartWith capitalLetterRegex
+      }
+    }
   }
 })
+
+private fun String.findWords() = Regex("[a-zA-Z]+").findAll(this).map { it.value }.toList()
+private fun String.findSeparators() = Regex("[^\\d\\w]+").findAll(this).map { it.value }.toList()
+
+private val capitalLetterRegex = "[A-Z]".toRegex()
